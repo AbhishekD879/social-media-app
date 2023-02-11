73752d9dd2585b871043a46c2c98b4b74e593368
Abhishek.Diwate <abhishek.diwate@ivycomptech.com>
2023-02-11 12:42:18 +0530

    project complete

---
+++ src/scenes/homePage/index.jsx
@@ -0,0 +1,14 @@
+import { Box} from "@mui/material";
+import Navbar from "scenes/navbar";
+// import Navbar from "./navbar/index.js";
+
+const HomePage = () => {
+  return (
+    <Box>
+        
+      <Navbar />
+    </Box>
+  );
+};
+
+export default HomePage;
